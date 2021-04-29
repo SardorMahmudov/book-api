6728ed97686ede6af7b02a3928aa1be876dc8d45
Sardor Mahmudov <ozdemeir09@gmail.com>
2021-04-29 16:19:54 +0500

    add User(finish)

---
+++ src/Controller/UserCreateAction.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+class UserCreateAction
+{
+    public function __invoke(): void
+    {
+        print 'Hello world';
+        exit();
+    }
+}
